b379fbf4a048d96f93968d4ce06f91a1586fc621
yash7803 <goswamiyash7803@gmail.com>
2025-12-06 16:21:04 +0530

    all done product and coures

--- server/routes/products.js
+++ server/routes/products.js
@@ -3,9 +3,11 @@ const productsCtrl = require("../controllers/products");
 const router = express.Router();
 
 router.route('/').post(productsCtrl.get_products);  
+router.route('/images').post(productsCtrl.get_products_images); 
 router.route('/add').post(productsCtrl.add_products);  
 router.route('/update').post(productsCtrl.update_products);  
 router.route('/delete').post(productsCtrl.delete_products);  
 router.route('/images/save').post(productsCtrl.saveProductImage); 
-router.route('/delete/thumbnail').post(productsCtrl.delete_products_thumbnail); 
+router.route('/images/delete').post(productsCtrl.deleteProductImage); 
+// router.route('/delete/thumbnail').post(productsCtrl.delete_products_thumbnail); 
 module.exports = router;
